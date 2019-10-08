f2ec026fd1085e4abe81b7e5abee7489e16003b3
Gledbury <gledbury4@gmail.com>
2019-10-08 15:18:09 -0700

    lab 3 done

---
+++ tuesday demo 1008/iterations/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region simple for loop
+            Console.WriteLine("For loop example");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            #endregion
+
+            #region nest for loops
+            Console.WriteLine("nested loop example");
+            Console.WriteLine("i, j\n----");
+            for (int i=0; i<3; i++)
+            {
+                for (int j=0; j<3; j++)
+                {
+                    Console.WriteLine("{0}, {1},", i, j);
+                    Console.WriteLine();
+                }
+            }
+            #endregion
+
+            #region foreach loop (definite)
+            Console.WriteLine("foreach loop example");
+            int[] myInts = { 1, 2, 3, 4, 5, };
+            for (int i = 0; i < myInts.Length; i++)
+            {
+                myInts[i] = i + 10;
+            }
+            foreach(int element in myInts)
+            {
+                Console.WriteLine(element);
+            }
+            #endregion
+
+            #region while loop example (indefinite)
+            Console.WriteLine("while loop example");
+            int count = 0;
+            while (count < myInts.Length)
+            {
+                Console.WriteLine("{0}", myInts[count]);
+                count++;
+            }
+            Console.WriteLine();
+            #endregion
+
+            #region do/while example
+            Console.WriteLine("Do/while example");
+            count = 0;
+            do
+            {
+                Console.WriteLine(myInts[count]);
+                count++;
+            } while (count < myInts.Length);
+            
+            #endregion
+        }
+    }
+}
